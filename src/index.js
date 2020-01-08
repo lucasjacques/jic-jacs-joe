7106f5ba6cd2203857bce01d71b98b326eb88c3f
Lucas Jacques <lucas.jacs@gmail.com>
2020-01-08 02:17:50 -0300

    TIP: added winning calculus fn + stop the game after winning

--- src/index.js
+++ src/index.js
@@ -2,18 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css' ;
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button 
-        className="square" 
-        onClick = {() => this.props.onClick()}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
-
+function Square(props) {
+  return (
+    <button 
+      className="square"
+      onClick={props.onClick}
+    >
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
@@ -23,13 +20,22 @@ class Board extends React.Component {
     this.state = {
       currentPlayer: null,
       squares: Array(9).fill(null),
+      xIsCurrentPlayer: true,
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsCurrentPlayer ? 'X' : 'O';
+    this.setState(
+      {
+        squares: squares,
+        xIsCurrentPlayer: !this.state.xIsCurrentPlayer,
+      }
+    );
   }
 
   renderSquare(i) {
@@ -40,25 +46,17 @@ class Board extends React.Component {
     );
   }
 
-  printSymbol() {
-    switch (this.state.currentPlayer) {
-      case 1 :
-        return 'X';
-      case 2 :
-        return 'O';
-      default:
-        return;
+  render() {      
+    const winner = calculateWinner(this.state.squares);
+    let status, currentPlayer;
+
+    if (winner) {
+      currentPlayer = generatePlayerText(this.state.xIsCurrentPlayer, true);
+      status = 'Winner: ' + currentPlayer;
+    } else {
+      currentPlayer = generatePlayerText(this.state.xIsCurrentPlayer);
+      status = 'Current player: ' + currentPlayer;
     }
-  }
-
-  move() {
-    let nextState = {};
-    nextState.currentPlayer = this.state.currentPlayer === 1 ? 2 : 1; 
-    this.setState(nextState);
-  }
-
-  render() {
-    const status = 'Current player: Player 1 (X)';
 
     return (
       <div>
@@ -105,3 +103,29 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+function generatePlayerText(xIsCurrentPlayer, isWinner=false) {
+  xIsCurrentPlayer = isWinner ? !xIsCurrentPlayer : xIsCurrentPlayer;
+  return `Player ${xIsCurrentPlayer ? '1' : '2'}  
+    (${xIsCurrentPlayer ? 'X' : 'O'})`;
+}
